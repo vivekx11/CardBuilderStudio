0b203ab6b69d98c8f4b67a241685b479240d93ac
Vivek Sawji <viveksawji06@gmail.com>
2026-06-07 18:27:44 +0530

    Fix formatting issues in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-
+// root 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
